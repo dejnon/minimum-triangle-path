aac27ded24aaa13638f92f0de59e59a6f4361b26
Daniel Kosalla <dejnon@gmail.com>
2018-06-19 17:49:00 +0200

    Refactor to use more functional style (Step 2)

--- src/main/scala/MinimumTrianglePath.scala
+++ src/main/scala/MinimumTrianglePath.scala
@@ -4,20 +4,14 @@ object MinimumTrianglePath {
   }
 
   def solve(triangle: Array[Array[Int]]): Int = {
-
-    val sums = triangle.map(_.clone)
-
-    sums.reduceRight((upperLevel, lowerLevel) => {
-      for( node <- (0 until (lowerLevel.length - 1))){
-        val upperNode = upperLevel(node)
-        val leftNode  = lowerLevel(node)
-        val rightNode = lowerLevel(node+1)
-
-        upperLevel(node) = upperNode + Math.min(leftNode, rightNode)
+    triangle.reduceRight((upperLevel, lowerLevel) => {
+      upperLevel.zipWithIndex.map{
+        case (upperNode, node) => {
+          val leftNode  = lowerLevel(node)
+          val rightNode = lowerLevel(node+1)
+          upperNode + Math.min(leftNode, rightNode)
+        }
       }
-      upperLevel
-    })
-
-    return sums(0)(0)
+    }).head
   }
 }
